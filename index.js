6c9d557176a8ddcd807d0342f401844b5b7db2ff
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2024-01-07 21:50:59 +0300

    aster

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+/*function myAccumulator(anArray){
+    for(let i=anArray[0];i<anArray.length;i++)
+    return anArray++;
+}*/
 // Code your solution here
+const batteryBatches2=batteryBatches.reduce(function(accumulator,element){
+  return element +  accumulator 
+},0)
